ffc00a2c681030eab59db4bdfc66aa905d987046
Felipe Costa <felipelena8@gmail.com>
2022-02-26 14:36:24 -0300

    Ejercicio 4- Carrito y contenedor de items

--- src/App.js
+++ src/App.js
@@ -2,49 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./css/style.css";
 import NavBar from "./componentes/NavBar";
+import "https://kit.fontawesome.com/b3e3212614.js";
+import ItemListContainer from "./componentes/ItemListContainer";
 function App() {
-  // let condition = true
-  // let result = null
-  // if (condition) {
-  //     result = 'verdadero'
-  // } else {
-  //     result = 'falso'
-  // }
-  // console.log(`El resultado es: ${condition ? 'verdadero': 'falso'}`)
-
-  // spread operator
-  // const array = ['b', 'c', 'd']
-  // let a = 'a'
-  // const newArray = [a, ...array]
-  // console.log(newArray);
-
-  // campos dinamicos
-  // let nombre = 'fede'
-  // const obj = {
-  //     'dni': 1231245,
-  //     ['id_' + nombre]: 1234567
-  // }
-  // console.log(obj);
-
-  // destructuring
-  // const obj = {
-  //     nombre: 'fede',
-  //     apellido: 'Osandon'
-  // }
-  // const { nombre: nom, apellido:ape} = obj
-  // console.log(nom, ape);
-
-  // asignacion en desestructuracion
-  //   const obj = {
-  //     nombre: 'fede',
-  //     apellido: 'Osandon'
-  // }
-  // const { nombre: nom, apellido, edad = 15 } = obj
-  //   console.log(edad);
-
   return (
     <div className="App">
       <NavBar />
+      <main>
+        <ItemListContainer />
+      </main>
     </div>
   );
 }
---
+++ src/componentes/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ItemListContainer() {
+  return (
+    <div className="container d-flex justify-content-around flex-wrap">
+      Futuro catalogo
+    </div>
+  );
+}
