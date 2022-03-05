40ae91b0b859cf2798f41110d34408aa662ff8cb
Felipe Costa <felipelena8@gmail.com>
2022-03-05 14:29:57 -0300

    Ejercicio del catalogo hecho con promise y useEffect

--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import productos from "../js/modelo.js";
+import { Toast } from "bootstrap";
+
+const task = new Promise((resolve, reject) => {
+  let condicion = true;
+  if (condicion) {
+    setTimeout(() => resolve(productos), 2000);
+  } else {
+    reject("400 - not found");
+  }
+});
 
 export default function ItemListContainer() {
-  const htmlProds = [];
+  const [loading, setLoading] = useState(true);
+  const [prods, setProds] = useState([]);
   const urlBase = "/images/productos/";
-  for (let prod of productos) {
-    htmlProds.push(
-      <Card
-        id={prod.id}
-        nombre={prod.nombre}
-        precio={prod.precio}
-        imagen={urlBase + prod.imagen}
-      />
-    );
-  }
-  console.log(htmlProds);
+  useEffect(() => {
+    task
+      .then((resp) => setProds(resp))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <div className="container d-flex justify-content-around flex-wrap">
-      {htmlProds}
+      {loading ? (
+        <div className="loading">
+          <div className="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        prods.map((prod, index) => (
+          <Card
+            key={prod.id}
+            id={prod.id}
+            nombre={prod.nombre}
+            precio={prod.precio}
+            imagen={urlBase + prod.imagen}
+          />
+        ))
+      )}
     </div>
   );
 }
