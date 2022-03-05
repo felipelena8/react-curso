bbff13c448611652ad0a956f06b6a9613098393b
Felipe Costa <felipelena8@gmail.com>
2022-03-05 13:04:20 -0300

    Ejercicio contador

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "./css/style.css";
-import NavBar from "./componentes/NavBar";
-import "https://kit.fontawesome.com/b3e3212614.js";
-import ItemListContainer from "./componentes/ItemListContainer";
+import logo from './logo.svg';
+import './App.css';
+import ItemsCount from "./componentes/ItemsCount";
+import "./scss/itemCount.scss";
+
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <main>
-        <ItemListContainer />
-      </main>
-    </div>
-  );
+  return <ItemsCount initial={1} stock={10}></ItemsCount>;
 }
 
 export default App;
--- src/componentes/ItemListContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ItemListContainer() {
-  return (
-    <div className="container d-flex justify-content-around flex-wrap">
-      Futuro catalogo
-    </div>
-  );
-}
